a6b3e7e7bb89a4eb9ba228665a1ac63b35d13c9a
Jin Su Ha <E-mail>
2022-09-27 17:10:35 +0900

    [하진수] 2022_09_24_05:05_채팅, 채팅룸 레이아웃, 자바클래스 추가

---
+++ app/src/main/java/com/example/pocket/class_/chat/RoomData.java
@@ -0,0 +1,45 @@
+package com.example.pocket.class_.chat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RoomData {
+
+    private String userName;
+    private String roomNumber;
+
+    public RoomData(String userName, String roomNumber) {
+        this.userName = userName;
+        this.roomNumber = roomNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String toJSON() {
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            jsonObject.put("username", getUserName());
+            jsonObject.put("roomNumber", getRoomNumber());
+
+            return jsonObject.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
