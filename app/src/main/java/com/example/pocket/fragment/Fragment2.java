b77b411eeaee109b93cbc828452c3e9a00091223
smhrd <minji09106@gmail.com>
2022-09-28 09:36:09 +0900

    [이민지] 2022_09_28_09:28_아이콘

--- app/src/main/java/com/example/pocket/fragment/Fragment2.java
+++ app/src/main/java/com/example/pocket/fragment/Fragment2.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -59,6 +60,7 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_chat_test, container, false);
     }
