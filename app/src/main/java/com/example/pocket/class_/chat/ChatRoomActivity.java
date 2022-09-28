7c79b5f924e631130d22ae1f90dff30367788b9f
smhrd <minji09106@gmail.com>
2022-09-28 15:34:38 +0900

    [이민지] 2022_09_28_15:34_채팅방리스트

---
+++ app/src/main/java/com/example/pocket/class_/chat/ChatRoomActivity.java
@@ -0,0 +1,16 @@
+package com.example.pocket.class_.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pocket.R;
+
+public class ChatRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+    }
+}
