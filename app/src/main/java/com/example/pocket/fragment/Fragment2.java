9ab65e0c7962b302efab29725e0d5e4110c777b3
smhrd <rjsqls456@naver.com>
2022-09-28 09:50:49 +0900

    김건빈 2022-09-28

--- app/src/main/java/com/example/pocket/fragment/Fragment2.java
+++ app/src/main/java/com/example/pocket/fragment/Fragment2.java
@@ -1,13 +1,16 @@
 package com.example.pocket.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.pocket.R;
+import com.example.pocket.activity.ChatActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,7 +62,18 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_chat_test, container, false);
+        View view = inflater.inflate(R.layout.activity_chat_test, container, false);
+
+        Button btn = view.findViewById(R.id.btnChatJoin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext().getApplicationContext(), ChatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
