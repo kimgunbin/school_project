cf411454e5aa12ffdce35e68e4303e137967305f
smhrd <rjsqls456@naver.com>
2022-09-30 16:35:28 +0900

    2022 09 30 frag

--- app/src/main/java/com/example/pocket/fragment/Fra_chat_S.java
+++ app/src/main/java/com/example/pocket/fragment/Fra_chat_S.java
@@ -2,10 +2,39 @@ package com.example.pocket.fragment;
 
 
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
 import androidx.fragment.app.Fragment;
 
+import com.example.pocket.R;
+import com.example.pocket.activity.LoginActivity;
+import com.example.pocket.activity.MainActivity_T;
+import com.example.pocket.class_.chat.ChatActivity;
 
 
 public class Fra_chat_S extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_chat_main, container, false);
+        Button btn = view.findViewById(R.id.enterBtn);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext().getApplicationContext(), ChatActivity.class);
+                startActivity(intent);
+            }
+        });
+// ㅎㅎ
+        return view;
+    }
 }
