f8ca719b9519ff5c7e7c71637a21b67c387a6b70
smhrd <rjsqls456@naver.com>
2022-10-07 16:18:43 +0900

    2022 10 7 게시판 수정중

--- app/src/main/java/com/example/pocket/class_/board/adapter/BoardVO.java
+++ app/src/main/java/com/example/pocket/class_/board/adapter/BoardVO.java
@@ -6,6 +6,16 @@ public class BoardVO {
     private String context;
     private String file;
     private String seq;
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -39,11 +49,12 @@ public class BoardVO {
     }
 
 
-    public BoardVO(String title, String context, String file,String seq) {
+    public BoardVO(String title, String context, String file,String seq,String code) {
         this.title = title;
         this.context = context;
         this.file = file;
         this.seq = seq;
+        this.code = code;
     }
 // private (내부 메서드(get/set)를 사용해서 접근 가능)
 
