5be78f45e8d38202d81c79b3c8dd048c90d50085
smhrd <hbj5656@naver.com>
2022-10-07 15:35:37 +0900

    [하진수] 2022-10-07_database 폴더 내 NodePostJSON 클래스 추가

---
+++ app/src/main/java/com/example/pocket/class_/database/NodePostJSON.java
@@ -0,0 +1,95 @@
+package com.example.pocket.class_.database;
+
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NodePostJSON extends AsyncTask {
+    Object o;
+
+    @Override
+    public Object doInBackground(Object[] objects) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+
+            HttpURLConnection con = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(objects[0].toString());
+                String queryStr = objects[1].toString();
+                con = (HttpURLConnection) url.openConnection();
+
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Cache-Control", "no-cache");
+                con.setRequestProperty("Content-Type", "application/json");
+                con.setRequestProperty("Accept", "text/html");
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                con.connect();
+
+                OutputStream outputStream = con.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+                writer.write(queryStr);
+                writer.flush();
+                writer.close();
+
+                InputStream stream = con.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                return buffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (con != null) {
+                    con.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+        this.o = o;
+    }
+
+    public Object getO() {
+        return o;
+    }
+
+    public void setO(Object o) {
+        this.o = o;
+    }
+}
