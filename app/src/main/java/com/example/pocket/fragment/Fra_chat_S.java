dd43c38ffe9f46b2f33b64ef91394ec8bbb0b43e
smhrd <minji09106@gmail.com>
2022-10-07 14:15:50 +0900

    [이민지] 20221007_14:15_CCTV 보관버튼

--- app/src/main/java/com/example/pocket/fragment/Fra_chat_S.java
+++ app/src/main/java/com/example/pocket/fragment/Fra_chat_S.java
@@ -22,30 +22,4 @@ import java.util.ArrayList;
 
 public class Fra_chat_S extends Fragment {
 
-    ArrayList<TeacherVO> data = new ArrayList<>();
-    ListView lv;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_title_s, container, false);
-        lv = view.findViewById(R.id.lv1);
-        data.add(new TeacherVO("선생님1","선생님"));
-        data.add(new TeacherVO("선생님2","선생님"));
-        data.add(new TeacherVO("선생님3","선생님"));
-        data.add(new TeacherVO("선생님4","선생님"));
-
-
-        TeacherAdapter adapter = new TeacherAdapter(
-                getContext().getApplicationContext(),
-                R.layout.teacher_list,
-                data);
-
-
-        // 5 ListView에 Adapter적용 시키기
-        lv.setAdapter(adapter);
-
-        return  view;
-
-    }
 }
--- app/src/main/java/com/example/pocket/fragment/Fra_law_S.java
+++ app/src/main/java/com/example/pocket/fragment/Fra_law_S.java
@@ -5,4 +5,6 @@ import androidx.fragment.app.Fragment;
 
 public class Fra_law_S extends Fragment {
 // 푸쉬용 주석
+
+
 }
