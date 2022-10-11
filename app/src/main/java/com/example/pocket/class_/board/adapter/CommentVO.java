07b0d30837b72eedbfa047dc5f07a2aea6a9d1b2
smhrd <minji09106@gmail.com>
2022-10-11 15:13:40 +0900

    [이민지] 20221011_15:13_댓글목록

--- app/src/main/java/com/example/pocket/class_/board/adapter/CommentVO.java
+++ app/src/main/java/com/example/pocket/class_/board/adapter/CommentVO.java
@@ -5,15 +5,25 @@ public class CommentVO {
     private String  seq;
     private String id;
     private String content;
+    private String date;
 
-    public CommentVO(String seq, String id, String content) {
+    public CommentVO(String seq, String id, String content, String date) {
         this.seq = seq;
         this.id = id;
         this.content = content;
+        this.date = date;
     }
 
     public String getSeq() {
         return seq;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public void setSeq(String seq) {
